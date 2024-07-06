cfa9651aca0a96827bb5de3388ab1b8246a74b72
Anuj Mehla <anujmehla@gmail.com>
2024-07-06 15:32:36 +0530

    backend basic code

---
+++ backend/Spoorthi/src/main/java/com/anuj/Spoorthi/SpoorthiApplication.java
@@ -0,0 +1,13 @@
+package com.anuj.Spoorthi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpoorthiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpoorthiApplication.class, args);
+	}
+
+}
---
+++ backend/Spoorthi/src/test/java/com/anuj/Spoorthi/SpoorthiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anuj.Spoorthi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpoorthiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
